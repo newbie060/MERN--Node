0477a72b5a1f3960e769156ff066634ede648a5e
newbie060 <poozagrg432@gmail.com>
2024-08-15 18:50:15 +0545

    push

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const connectToDatabase = require('./database/index.js')
 const app = express()
 
+connectToDatabase()
 
 app.get("/",(req,res)=>{
     console.log(req)
-    res.json({
+    res.status(200).json({
         message: "This is home page"
     })
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("NodeJS project has started")
 })
+
+//mongodb+srv://poozagrg432:<password>@cluster0.gdjpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
